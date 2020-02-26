e30f4fc806d397968a17470aaea401b969638239
MrKhantee <mrkhantee@gmail.com>
2020-02-26 22:01:21 +0700

    Change androidx to use Activity. Add screenshot.

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Convert your images to pdf files in Android.
 
 - Convert multiple images to pdf files.
 - Storage location is Files/Internal Files/Download which is challenging for user to open pdf files. Therefore, direction in some way into file, which contains pdf files, will be a good shortcut.
+
+![Screenshot](Screenshot_2020-02-26-21-53-54.png)
+![Screenshot](Screenshot_2020-02-26-21-54-34.png)
--- app/src/main/java/com/example/imgtopdf/MainActivity.java
+++ app/src/main/java/com/example/imgtopdf/MainActivity.java
@@ -1,41 +1,20 @@
 package com.example.imgtopdf;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+import android.app.*;
+import android.content.*;
+import android.database.*;
+import android.graphics.*;
+import android.graphics.pdf.*;
+import android.net.*;
+import android.os.*;
+import android.provider.*;
+import android.text.*;
+import android.view.*;
+import android.widget.*;
+import java.io.*;
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.pdf.PdfDocument;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
-import pub.devrel.easypermissions.EasyPermissions;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity
+{
 
     ImageView imageView;                // image that will be converted into .pdf
     private static final int CODE = 1;  // CODE for onActivityResult
@@ -44,51 +23,61 @@ public class MainActivity extends AppCompatActivity {
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+	{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         imageView = findViewById(R.id.ImageView);
         fileName = findViewById(R.id.fileName);
 
-        // granting permission
-        String[] galleryPermissions = {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
-        if (EasyPermissions.hasPermissions(this, galleryPermissions)) {
-            Log.e("permission", "permissions are granted");
-        } else {
-            EasyPermissions.requestPermissions(this, "Access for storage",
-                    101, galleryPermissions);
-        }
+		/*
+		 // granting permission
+		 String[] galleryPermissions = {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+		 if (EasyPermissions.hasPermissions(this, galleryPermissions)) {
+		 Log.e("permission", "permissions are granted");
+		 } else {
+		 EasyPermissions.requestPermissions(this, "Access for storage",
+		 101, galleryPermissions);
+		 }
+		 */
 
         // TextWatcher is going to track fileName which is user input for pdf file name.
         // User will be informed always while EditText (filename) is changing.
         fileName.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+				@Override
+				public void beforeTextChanged(CharSequence s, int start, int count, int after)
+				{
 
-            }
+				}
 
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-               checkFileName();
-            }
+				@Override
+				public void onTextChanged(CharSequence s, int start, int before, int count)
+				{
+					checkFileName();
+				}
 
-            @Override
-            public void afterTextChanged(Editable s) {
-                checkFileName();
-            }
-        });
+				@Override
+				public void afterTextChanged(Editable s)
+				{
+					checkFileName();
+				}
+			});
     }
 
-    public boolean checkFileName() {
+    public boolean checkFileName()
+	{
         TextView validity = findViewById(R.id.validity);
         Button saveButton = findViewById(R.id.saveButton);
-        if (fileName.getText().toString().endsWith(".pdf")) {
+        if (fileName.getText().toString().endsWith(".pdf"))
+		{
             validity.setTextColor(Color.GREEN);
             validity.setText("Valid");
             saveButton.setVisibility(View.VISIBLE);
             return true;
-        } else {
+        }
+		else
+		{
             validity.setTextColor(Color.RED);
             validity.setText("INVALID!");
             saveButton.setVisibility(View.INVISIBLE);
@@ -96,20 +85,24 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void selectImage(View v) {
+    public void selectImage(View v)
+	{
         Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(intent, CODE);
     }
 
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (requestCode == CODE && resultCode == RESULT_OK && data != null) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+        if (requestCode == CODE && resultCode == RESULT_OK && data != null)
+		{
             Uri selectedImageURI = data.getData();
 
             String[] filePath = {MediaStore.Images.Media.DATA};
             Cursor cursor = getContentResolver().query(selectedImageURI, filePath, null, null, null);
-            if (cursor == null) {
+            if (cursor == null)
+			{
                 Toast.makeText(this, "Cursor Error", Toast.LENGTH_SHORT);
                 return;
             }
@@ -122,11 +115,12 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, path.toString(), Toast.LENGTH_SHORT).show();
             imageView.setImageBitmap(bitmap);
             makePDF(bitmap);
-            }
+		}
         super.onActivityResult(requestCode, resultCode, data);
     }
 
-    public void makePDF(Bitmap bitmap) {
+    public void makePDF(Bitmap bitmap)
+	{
         pdfDocument = new PdfDocument();
         PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(bitmap.getWidth(), bitmap.getHeight(), 1).create();
         PdfDocument.Page page = pdfDocument.startPage(pageInfo);
@@ -137,35 +131,43 @@ public class MainActivity extends AppCompatActivity {
         canvas.drawBitmap(bitmap, 0, 0, null);
         pdfDocument.finishPage(page);
 
-        if (fileName.getText().toString().isEmpty()) {
+        if (fileName.getText().toString().isEmpty())
+		{
             Toast.makeText(this, "You need to enter file name as follow\nyour_fileName.pdf", Toast.LENGTH_SHORT).show();
         }
     }
 
 
-    public void saveFile(View v) {
-        if (pdfDocument == null) {
+    public void saveFile(View v)
+	{
+        if (pdfDocument == null)
+		{
             Toast.makeText(this, "null pointer exception", Toast.LENGTH_SHORT).show();
             System.out.println("Null pointer => pdfDocument");
             return;
         }
         File root = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "ImgToPDF");
         boolean isDirectoryCreated = root.exists();
-        if (!isDirectoryCreated) {
+        if (!isDirectoryCreated)
+		{
             isDirectoryCreated = root.mkdir();
         }
-        if (checkFileName()) {
+        if (checkFileName())
+		{
             String userInputFileName = fileName.getText().toString();
             File file = new File(root, userInputFileName);
-            try {
+            try
+			{
                 FileOutputStream fileOutputStream = new FileOutputStream(file);
                 pdfDocument.writeTo(fileOutputStream);
-            } catch (IOException e) {
+            }
+			catch (IOException e)
+			{
                 e.printStackTrace();
             }
             pdfDocument.close();
         }
-        Toast.makeText(this, "Successful! PATH:\n" +"Internal Storage/" + Environment.DIRECTORY_DOWNLOADS, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Successful! PATH:\n" + "Internal Storage/" + Environment.DIRECTORY_DOWNLOADS, Toast.LENGTH_SHORT).show();
     }
 
 }
